dfc31615ee4d6dbaf5348dd5090b4b3edd603c9f
Lawrence Kesteloot <lk@teamten.com>
2013-01-15 10:16:12 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+Gpp is a Go pre-processor that inlines template files.
---
+++ gpp.go
@@ -0,0 +1,126 @@
+// Copyright 2013 Lawrence Kesteloot
+
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/printer"
+	"go/token"
+	"io/ioutil"
+	"os"
+)
+
+func parseIncludeCall(s ast.Stmt) (filename string, ok bool) {
+	e, ok := s.(*ast.ExprStmt)
+	if ok {
+		c, ok := e.X.(*ast.CallExpr)
+		if ok {
+			i, ok := c.Fun.(*ast.Ident)
+			if ok {
+				if i.Name == "include" {
+					arg0, ok := c.Args[0].(*ast.BasicLit)
+					if ok && arg0.Kind == token.STRING {
+						filename := arg0.Value
+						// Strip out quotes.
+						filename = filename[1:len(filename) - 1]
+						return filename, true
+					}
+				}
+			}
+		}
+	}
+	ok = false
+	return
+}
+
+func findIncludeCall(b *ast.BlockStmt) (filename string, index int, ok bool) {
+	var s ast.Stmt
+
+	for index, s = range b.List {
+		filename, ok = parseIncludeCall(s)
+		if ok {
+			return
+		}
+	}
+	ok = false
+	return
+}
+
+func makeWriteStmt(text []byte) ast.Stmt {
+	e := &ast.ExprStmt{
+		X: &ast.CallExpr{
+			Fun: &ast.SelectorExpr{
+				X: &ast.Ident{
+					Name: "f",
+				},
+				Sel: &ast.Ident {
+					Name: "Write",
+				},
+			},
+			Args: []ast.Expr{
+				&ast.CallExpr{
+					Fun: &ast.ParenExpr{
+						X: &ast.ArrayType{
+							Elt: &ast.Ident {
+								Name: "byte",
+							},
+						},
+					},
+					Args: []ast.Expr{
+						&ast.BasicLit{
+							Kind: token.STRING,
+							Value: "`" + string(text) + "`",
+						},
+					},
+				},
+			},
+		},
+	}
+	return e
+}
+
+func parseFile(filename string) *ast.BlockStmt {
+	template, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("Cannot read file \"%s\" (%s)\n", filename, err)
+		os.Exit(1)
+	}
+
+	b := &ast.BlockStmt{}
+
+	b.List = append(b.List, makeWriteStmt(template))
+
+	return b
+}
+
+func processNode(n ast.Node) bool {
+	b, ok := n.(*ast.BlockStmt)
+	if ok {
+		filename, index, ok := findIncludeCall(b)
+		if ok {
+			b.List[index] = parseFile(filename)
+			return false
+		}
+	}
+
+	return true
+}
+
+func main() {
+	fset := token.NewFileSet() // positions are relative to fset
+
+	// Parse file.
+	f, err := parser.ParseFile(fset, "example.gt", nil, parser.ParseComments)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	/// ast.Print(fset, f)
+	ast.Inspect(f, processNode)
+	/// ast.Print(fset, f)
+
+	printer.Fprint(os.Stdout, fset, f)
+}
